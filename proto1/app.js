22643abb9a54e2d864ada884ba5c7f16948efcbd
azki <azki@azki.org>
2013-09-24 00:41:49 +0900

    script 추가만으로 UI 구성..

--- proto1/app.js
+++ proto1/app.js
@@ -1,4 +1,4 @@
-/*jslint regexp:false,nomen:false*/
+/*jslint regexp:false,nomen:false,white:false*/
 /*global exports,require,process,__dirname*/
 
 var fs = require('fs');
@@ -7,40 +7,70 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 var io = require('socket.io').listen(server, {
-	log: true
+    log: true
 });
 server.listen(12345);
-app.get('/', function (req, res) {
-	res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-	res.header('Pragma', 'no-cache');
-	res.header('Expires', '0');
-	res.header('Content-Type', 'text/html');
-	fs.readFile(__dirname + '/index.html', function (err, data) {
-		if (err) {
-			console.log('cannot read html file', err);
-			return;
-		}
-		res.end(data);
-	});
+app.get('/', function(req, res){
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.header('Pragma', 'no-cache');
+    res.header('Expires', '0');
+    res.header('Content-Type', 'text/html');
+    fs.readFile(__dirname + '/index.html', function(err, data){
+        if (err) {
+            console.log('cannot read html file', err);
+            return;
+        }
+        res.end(data);
+    });
 });
-app.get('/io.js', function (req, res) {
-	res.header('Content-Type', 'application/x-javascript');
-	fs.readFile(__dirname + '/io.js', function (err, data) {
-		if (err) {
-			console.log('cannot read js file', err);
-			return;
-		}
-		res.end(data);
-	});
+app.get('/chat.js', function(req, res){
+    res.header('Content-Type', 'application/x-javascript');
+    fs.readFile(__dirname + '/io.js', function(err, data1){
+        if (err) {
+            console.log('cannot read io.js file', err);
+            return;
+        }
+        fs.readFile(__dirname + '/ui.js', function(err, data2){
+            if (err) {
+                console.log('cannot read ui.js file', err);
+                return;
+            }
+            res.send([data1, data2].join("\n"));
+        });
+    });
 });
 
-//io.set('transports', ['websocket', 'xhr-polling', 'jsonp-polling', 'htmlfile']);
-io.of('/socket').on('connection', function (socket) {
-	var sockId = socket.id, addr = socket.handshake.address.address;
-	console.log('hello');
-	socket.emit('hello', 'hello');
-	socket.on('hi', function (msg) {
-		console.log('hi');
-		socket.emit('hi', 'hi');
-	});
+var socketMap = {};
+
+function broadcast(name, value){
+    for (var id in socketMap) {
+        if (socketMap.hasOwnProperty(id)) {
+            try {
+                socketMap[id].emit(name, value);
+            } 
+            catch (err) {
+                console.error("webSockets emit error", err);
+            }
+        }
+    }
+}
+
+io.of('/chat').on('connection', function(socket){
+    var sockId = socket.id, addr = socket.handshake.address.address;
+    socketMap[sockId] = socket;
+    broadcast('join', {
+        writer: addr
+    });
+    socket.on('msg', function(msg){
+        broadcast('msg', {
+            writer: addr,
+            text: msg.text
+        });
+    });
+    socket.on('disconnect', function(){
+        delete socketMap[sockId];
+        broadcast('leave', {
+            writer: addr
+        });
+    });
 });
---
+++ proto1/ui.js
@@ -0,0 +1,57 @@
+/*jslint regexp:false,nomen:false*/
+/*global io4chat*/
+(function () {
+	var socket, wrapDiv, chatDiv, chatInput;
+	socket = io4chat.connect("http://azki.org:12345/chat");
+	if (document.body) {
+		wrapDiv = document.createElement('div');
+		chatDiv = document.createElement('div');
+		chatInput = document.createElement('input');
+		
+		chatDiv.style.background = "#eef";
+		chatDiv.style.width = "360px";
+		chatDiv.style.height = "360px";
+		chatDiv.style.overflow = "scroll";
+		
+		chatInput.style.background = "#fee";
+		chatInput.style.width = "320px";
+		chatInput.onkeydown = function (event) {
+			if (event.keyCode === 13) {
+				socket.emit("msg", {
+					text: chatInput.value
+				});
+				chatInput.value = "";			
+				return false;
+			}
+		};
+		wrapDiv.appendChild(chatDiv);
+		wrapDiv.appendChild(chatInput);
+		document.body.appendChild(wrapDiv);
+	} else {
+		//TODO
+		alert("TODO");
+	}
+	socket.on("connect", function () {
+		console.log("connect");
+	});
+	socket.on("disconnect", function () {
+		console.log("disconnect");
+	});
+	socket.on("join", function (data) {
+		var msgP = document.createElement("p");
+		msgP.style.color = "blue";
+		msgP.innerHTML = "join : " + data.writer;
+		chatDiv.appendChild(msgP);
+	});
+	socket.on("leave", function (data) {
+		var msgP = document.createElement("p");
+		msgP.style.color = "red";
+		msgP.innerHTML = "leave : " + data.writer;
+		chatDiv.appendChild(msgP);
+	});
+	socket.on("msg", function (data) {
+		var msgP = document.createElement("p");
+		msgP.innerHTML = data.writer + " : " + data.text.replace(/</g, "&lt;");
+		chatDiv.appendChild(msgP);
+	});
+}());
